c79edd63f98e77bfb44d3c4af0ccd0c8b9189532
ojiah barnabas ogirima <ogisojiah@gmail.com>
2024-07-04 14:33:43 +0100

    initial Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+// function addNum(number) {
+//     let sum = 0;
+//     for (let i = 0; i <= number; i++) {
+//         sum = sum + i;
+//     }
+//     return sum;
+// }
+// console.log(addNum(85))
+
+function addEvenNum(number) {
+    let sum = 0;
+    for (let i = 0; i <= number; i=i+2) {
+     if (number%2!==0) {
+        return console.log('not an even number')
+     }
+     else{
+    sum=sum+i
+     }
+    }
+    return sum;
+}
+console.log(addEvenNum(12))
+
