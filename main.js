f2311733ca25752f9f500fde033d7985f16ccfb4
ojiah barnabas ogirima <ogisojiah@gmail.com>
2024-07-04 15:29:17 +0100

    i addedd comments

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
-// function addNum(number) {
-//     let sum = 0;
-//     for (let i = 0; i <= number; i++) {
-//         sum = sum + i;
-//     }
-//     return sum;
-// }
-// console.log(addNum(85))
+ //function addNum(number) {
+ // let sum = 0;
+//for (let i = 0; i <= number; i++) {
+// sum = sum + i;
+  //  }
+    // return sum;
+ //}
+ //console.log(addNum(85))
 
-function addEvenNum(number) {
-    let sum = 0;
+//function addEvenNum(number) {
+    
+let sum = 0;
     for (let i = 0; i <= number; i=i+2) {
      if (number%2!==0) {
         return console.log('not an even number')
